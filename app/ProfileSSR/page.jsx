6a1b2901cd9b33fe5b8cf4395a26657a55e4c95f
maxo2727 <maximiliano.valdivia27@gmail.com>
2025-10-05 20:23:34 -0300

    feat: connected login with backend auth0 logic

--- app/ProfileSSR/page.jsx
+++ app/ProfileSSR/page.jsx
@@ -4,11 +4,28 @@ export default async function UserInfo() {
     const session = await auth0.getSession();
     const user = session?.user;
 
+    let userData = null;
+    if (user) {
+        try {
+            console.log('Fetching user data with access token:', session.tokenSet.accessToken);
+            const res = await fetch('http://localhost:3003/users/profile', {
+                headers: {
+                    Authorization: `Bearer ${session.tokenSet.accessToken}`
+                }
+            })
+            userData = await res.json();
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    }
+
     return (
         <div>
             {user ? (
                 <>
                     <p>Welcome, {user.name}!</p>
+                    <p>access token: {session.tokenSet.accessToken}</p>
+                    <p>Id token: {session.tokenSet.idToken}</p>
                     <a href="/auth/logout">Logout</a>
                 </>
             ) : (
