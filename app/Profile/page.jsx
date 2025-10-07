0aa8daa99df89d55d38ebbde2939ca1af998b714
maxo2727 <maximiliano.valdivia27@gmail.com>
2025-10-06 21:33:07 -0300

    feat: login and signup profile flow

--- app/Profile/page.jsx
+++ app/Profile/page.jsx
@@ -4,9 +4,9 @@ import { useUser } from '@auth0/nextjs-auth0';
 
 export default function ProfilePage() {
     const { user, error, isLoading } = useUser();
+    console.log('User object:', user);
 
     if (isLoading) return <div>Loading...</div>;
-    // if (error) return <div>{error.message}</div>;
 
     return (
         <div>
